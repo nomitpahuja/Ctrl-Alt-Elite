bb88175268b5d8257b8da6408a29de11674e6563
OutrageousStop <ankitband@gmail.com>
2019-03-11 15:20:42 +0530

    changes

--- generator.py
+++ generator.py
@@ -5,12 +5,14 @@ from MLGen import generator
 def giveNames(num = 12):
     return {"names": (make_name(num) + generator(num))}
 
-def givebacknames(word = ""):
+if __name__ == "__main__":
+    word = input("Enter word: ")
     names = giveNames()["names"]
     prefix = names[:12]
     postfix = names[12:24]
-    gen = []
-    gen += word + prefix[i]
-    gen += prefix[i][:-len(prefix[i]) // 2] + word + prefix[i][len(prefix[i]) // 2 : ]
-    gen += postfix[i] + word
-    return gen
+    for i in range(9):
+        print(word + prefix[i])
+    for i in range(9, 12):
+        print(prefix[i][:-len(prefix[i]) // 2] + word + prefix[i][len(prefix[i]) // 2 : ])
+    for i in range(0,12):
+        print(postfix[i] + word)

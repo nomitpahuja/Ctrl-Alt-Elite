e9f13ec1f95048b335003ec57dc872d58522a58f
OutrageousStop <ankitband@gmail.com>
2019-03-11 08:40:10 +0530

    word generation

--- generator.py
+++ generator.py
@@ -1,10 +1,18 @@
 from name_maker import make_name
 import sys
-sys.path.insert(0, '/home/ankit/Desktop/python/Hackgrid/new files/Ctrl-Alt-Elite/scripts/')
 from MLGen import generator
 
 def giveNames():
     return {"names": (make_name(10) + generator(10))}
 
 if __name__ == "__main__":
-    print(giveNames())
+    word = input("Enter word: ")
+    names = giveNames()["names"]
+    prefix = names[:10]
+    postfix = names[10:20]
+    for i in range(7):
+        print(word + prefix[i])
+    for i in range(7, 10):
+        print(prefix[i][:-len(prefix[i]) // 2] + word + prefix[i][len(prefix[i]) // 2 : ])
+    for i in range(3,10):
+        print(postfix[i] + word)
